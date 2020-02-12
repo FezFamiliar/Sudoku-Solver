254808c46b10678bbfaec345474808542e7ebbda
root <root@localhost.localdomain>
2020-02-12 16:00:27 +0000

    Basic grid

--- Sudoku.c
+++ Sudoku.c
@@ -1,9 +1,26 @@
-#include <stdlib.h>
+#include <stdio.h>
+
+const int n = 9;
 
 
+void print_grid(int grid[n][n]){
+
+	for(size_t i = 0;i < n;i++){
+		for(size_t j = 0;j < n;j++){
+			printf("%d ",grid[i][j]);
+		}
+		printf("\n");
+	}
+}
 
 int main(){
 
+	int grid[n][n];
+	for(size_t i = 0;i < n;i++)
+		for(size_t j = 0;j < n;j++)
+			grid[i][j] = 0;
 
+
+	print_grid(grid);
 	return 0;
 }
